20a653f598a38fe51723bfacbd6c267a5da6db25
Kevin Loftus <mrkevinjloftus@gmail.com>
2020-05-09 23:45:39 -0400

    Merge conflict, adding back shared module

---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnderConstructionComponent } from '../components/under-construction/under-construction.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MarkdownComponent } from '../components/markdown/markdown.component';
+
+const MODULES = [
+  CommonModule,
+  ReactiveFormsModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatSnackBarModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatListModule,
+  MatProgressSpinnerModule
+];
+
+const COMPONENTS = [
+  UnderConstructionComponent,
+  MarkdownComponent
+];
+
+@NgModule({
+  declarations: [...COMPONENTS],
+  imports: [...MODULES],
+  exports: [...MODULES, ...COMPONENTS]
+})
+export class SharedModule { }
