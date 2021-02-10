72ff3849ef70d646ff1655cb0f156506dff29630
Kevin Loftus <mrkevinjloftus@gmail.com>
2021-02-09 20:37:35 -0500

    Creating module

---
+++ src/app/pages/metrix/metrix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metrix',
+  templateUrl: './metrix.component.html',
+  styleUrls: ['./metrix.component.scss']
+})
+export class MetrixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/metrix/metrix.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MetrixComponent } from './metrix.component';
+
+
+
+@NgModule({
+  declarations: [MetrixComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MetrixModule { }
