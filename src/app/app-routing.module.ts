1b5d3b50e0d93e8affeb90100b55b74bd381a5d0
Kevin Loftus <mrkevinjloftus@gmail.com>
2023-01-06 15:29:17 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChoresComponent } from './pages/chores/Chores.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { AuthGuard } from './shared/auth/auth-guard';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home' } },
   { path: 'blog', loadChildren: () => import('./pages/blog/blog.module').then(m => m.BlogModule), data: { title: 'Blog' } },
   { path: 'projects', component: ProjectsComponent, data: { title: 'Projects' } },
+  { path: 'chores', component: ChoresComponent, data: { title: 'Chore Tracker' } },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule), data: { title: 'Login' } },
   { path: '**', redirectTo: '/' }
 ];
